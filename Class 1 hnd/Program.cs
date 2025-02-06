31c1297c98e8a3f8ee67b0c31c7ff3bba5fcf7b1
shang <shang@SHANGEERTH_KING>
2025-02-06 12:03:39 +0530

    Add project files.

---
+++ Class 1 hnd/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+
+
+int  myage = 23;
+Console.WriteLine(myage);
+
+string esoft= "hi";
+Console.WriteLine(esoft);
+
+string full_name = "esoft";
+string last_name = "metro";
+
+Console.WriteLine(full_name + last_name);
+
+string full = "esoft"+"  "+"idiotr";
+Console.WriteLine(full);                            
+
+ int a = 78, b = 89, c = 89;
+string aa = "a", bb = "b", cc = "c";                        
+
+Console.WriteLine(a + b - c + aa + bb + cc);
+Console.WriteLine(a + b + c / a * b - c);
+Console.WriteLine(aa + bb + cc);  
+
+int p, o, i;
+p = o = i=580;
+Console.WriteLine(p + o + i);
+
+double asi = 6.3;
+Console.WriteLine(asi);         
+
+int ko = 65;
+double koi = (double)ko;
+Console.WriteLine(koi); */
+
+
+int ko = 89;
+double hello=ko;
+Console.WriteLine(typeof(int));
+Console.WriteLine(typeof(double));
+
+int myint = 10;
+double mydouble = 5.26;
+bool symbol = true;
+
+Console.WriteLine(Convert.ToString(myint));
+Console.WriteLine(Convert.ToDouble(myint));
+Console.WriteLine(Convert.ToInt32(myint));
+Console.WriteLine(Convert.ToString(myint));
+
+Console.WriteLine("enter ur age");
+string age = Console.ReadLine();
+Console.WriteLine("ur age is " + age);
